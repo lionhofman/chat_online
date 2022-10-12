4aa03d58eb1d0c349896eaa2af3d78df7ada4502
Lion Hofman <lion.hofman@posthaus.com.br>
2022-10-12 10:35:09 -0300

    ✨feat:reset text

--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -10,33 +10,45 @@ class TextComposer extends StatefulWidget {
 
 class _TextComposerState extends State<TextComposer> {
   bool _isComposing = false;
+  final TextEditingController _textController = TextEditingController();
+  void _resetText() {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Container(
-        child: Row(
-          children: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.photo_camera)),
-            Expanded(
-                child: TextField(
-              decoration: const InputDecoration.collapsed(
-                  hintText: 'Enviar uma Mensagem'),
-              onChanged: (text) {
-                setState(() {
-                  _isComposing = text.isNotEmpty;
-                });
-              },
-              onSubmitted: (text) {
-                widget.sendMessage(text);
-              },
-            )),
-            IconButton(
-                onPressed: _isComposing ? () {} : null,
-                icon: const Icon(Icons.send)),
-          ],
-        ),
+      child: Row(
+        children: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.photo_camera)),
+          Expanded(
+              child: TextField(
+            decoration: const InputDecoration.collapsed(
+                hintText: 'Enviar uma Mensagem'),
+            controller: _textController,
+            onChanged: (text) {
+              setState(() {
+                _isComposing = text.isNotEmpty;
+              });
+            },
+            onSubmitted: (text) {
+              widget.sendMessage(text);
+              _resetText();
+            },
+          )),
+          IconButton(
+              onPressed: _isComposing
+                  ? () {
+                      widget.sendMessage(_textController.text);
+                      _resetText();
+                    }
+                  : null,
+              icon: const Icon(Icons.send)),
+        ],
       ),
     );
   }
